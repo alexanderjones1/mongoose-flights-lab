8c171774290106a96561518b149f7c528cd4d218
alex-jones <abjhoops@gmail.com>
2023-01-10 07:45:16 -0700

    add style sheets, and index.ejs view, update routes and add inex function in controllers

--- routes/flights.js
+++ routes/flights.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
-import * flightsCtrl from '../controllers/flights.js'
+import * as flightsCtrl from '../controllers/flights.js'
 
 const router = Router()
 
+// GET localhost:3000/flights
+router.get('/', flightsCtrl.index)
 // GET /flights/new
 router.get('/new', flightsCtrl.new)
+// POST /movies
+router.post('/', flightsCtrl.create)
 
 export {
   router
